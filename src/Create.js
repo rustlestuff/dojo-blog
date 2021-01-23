6018f9ab4efd58bac0de7e7618553b2690938ace
Chantal van de Velde <ccvandevelde@outlook.com>
2021-01-23 11:21:03 +0100

    Completed project.

---
+++ src/Create.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('mario')
+    const [isLoading, setIsLoading] = useState(false)
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsLoading(true)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            setIsLoading(false)
+            history.push('/')
+        })
+    }
+
+    return ( 
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Body</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+
+                <label>Author</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+
+                { isLoading ? <button disabled>Adding blog...</button> : <button>Add Blog</button>}
+
+                <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p>
+            </form>
+        </div>
+     );
+}
+ 
+export default Create;
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import useFetch from './useFetch'
+import BlogList from './BlogList'
+
+const Home = () => {
+	const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs')
+
+	return (
+		<div className="home">
+			{error && <div>{ error }</div>}
+			{isLoading && <div>Loading...</div>}
+			{blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+		</div>
+	)
+}
+
+export default Home
